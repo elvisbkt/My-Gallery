ceddc26e3f973255d8e3fee584b81f79b3a38bbc
elvisbkt <47518658+elvisbkt@users.noreply.github.com>
2019-08-26 15:10:14 +0300

    allow debugging

--- gallery/settings.py
+++ gallery/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i7i#(*nrxkgn#pt#m18rf$3!f(wn4zr-%ercnvww85+0qqqx11'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
